0e594e2864b4b3d2d1ceef1981c4fd7e58c28275
alon55 <alonzager5@gmail.com>
2023-05-17 15:14:03 +0300

    calculate 1 book price without discount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cleanCodeQuantity, setCleanCodeQuantity] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const calculateBooksPrice = () => {
+    if (cleanCodeQuantity === 1) { setTotalPrice(50) }
+  }
+
   return (
     <div className="App">
       <h3>
         Book price calculator - TDD
       </h3>
+      <label htmlFor="clean-code">Clean Code <input type="number" min={0} id="clean-code" value={cleanCodeQuantity} onChange={e => setCleanCodeQuantity(Number(e.target.value))}></input></label>
+      <button onClick={() => calculateBooksPrice()}>Calculate Total Price</button>
+      <h4>{`Total price: ${totalPrice}`}</h4>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 
@@ -8,6 +8,19 @@ describe("Book price calculator - Tests", () => {
     render(<App />);
     const title = screen.getByRole('heading', { level: 3 });
     expect(title.innerHTML).toBe('Book price calculator - TDD');
+  });
+
+  test("1 book - without discount", async () => {
+    render(<App />)
+
+    const input = screen.getByLabelText('Clean Code')
+    fireEvent.change(input, { target: { value: 1 } })
+
+    const calculatePrice = screen.getByRole('button', { name: /Calculate Total Price/i });
+    fireEvent.click(calculatePrice)
+
+    const totalPrice = screen.getByRole('heading', { level: 4 });
+    expect(totalPrice.innerHTML).toBe('Total price: 50');
   });
 
 });
